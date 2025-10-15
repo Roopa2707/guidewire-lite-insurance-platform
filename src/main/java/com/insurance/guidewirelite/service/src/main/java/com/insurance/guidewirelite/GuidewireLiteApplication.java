2387d6ed646c28fdf9969b27789c30f768f44211
Roopa2707 <rupareddynandikonda2001@gmail.com>
2025-10-15 08:51:05 -0500

    Added main application starter file

---
+++ src/main/java/com/insurance/guidewirelite/service/src/main/java/com/insurance/guidewirelite/GuidewireLiteApplication.java
@@ -0,0 +1,12 @@
+
+package com.insurance.guidewirelite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuidewireLiteApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GuidewireLiteApplication.class, args);
+    }
+}
